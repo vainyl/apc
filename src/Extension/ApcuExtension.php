d1821a84e214cca30d5da321859bfe4663796359
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-08-18 12:33:40 +0300

    Added Extension registration

---
+++ src/Extension/ApcuExtension.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Apcu-Bridge
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Apcu\Extension;
+
+use Vainyl\Core\Extension\AbstractFrameworkExtension;
+
+/**
+ * Class ApcuExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class ApcuExtension extends AbstractFrameworkExtension
+{
+}
